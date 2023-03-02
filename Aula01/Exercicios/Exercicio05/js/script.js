3d9c0afd9ceb200898c5e7e7f1b1166da6e30a2c
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-03-02 17:02:52 -0300

    Update

--- Aula01/Exercicios/Exercicio05/js/script.js
+++ Aula01/Exercicios/Exercicio05/js/script.js
@@ -1,5 +1,4 @@
 const changeTitle = () => {
     let result = document.getElementById('input').value
-    let title = document.getElementsByTagName(title)
-    title = result;
+    document.title = result;
 }
