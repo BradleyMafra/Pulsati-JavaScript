3c74c65b0f0cead2247255bc17e57a4af8459a8f
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-03-02 17:03:09 -0300

    Update

---
+++ Aula01/Exercicios/Exercicio07/js/script.js
@@ -0,0 +1,7 @@
+const esconder = () => {
+    let button = document.getElementById('btn').hidden = false;
+}
+
+const desesconder = () => {
+    let btn = document.getElementById('btn').hidden = true;
+}
