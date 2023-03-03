d7c907b7b98cb4ab71824bf337ad21a4de14d705
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-03-03 16:34:00 -0300

    Update

--- Aula01/Exercicios/Exercicio-Site/js/script.js
+++ Aula01/Exercicios/Exercicio-Site/js/script.js
@@ -1,4 +1,3 @@
-
 const novaTransacaoBtn = document.querySelector('.new-transition button');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-btn');
@@ -6,13 +5,17 @@ const addBtn = document.querySelector('.add-btn');
 const descricaoInput = document.querySelector('#descricao');
 const valorInput = document.querySelector('#valor');
 const dataInput = document.querySelector('#data');
+const depositar = document.getElementById('depositar');
+const sacar = document.getElementById('sacar');
 
 novaTransacaoBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
+  modal.style.display = 'flex';
 });
 
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-
+const addTrasacao = () => {
+  
+}
